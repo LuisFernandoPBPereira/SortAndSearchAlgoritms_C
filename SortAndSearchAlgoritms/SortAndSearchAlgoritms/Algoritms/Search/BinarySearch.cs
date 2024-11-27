35eb685efab25971b42cce9b2adf889dbcf6022c
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-11-27 08:43:33 -0300

    feat: Criando algoritmo Binary Search

---
+++ SortAndSearchAlgoritms/SortAndSearchAlgoritms/Algoritms/Search/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿namespace SortAndSearchAlgoritms.Algoritms.Search;
+
+public static class BinarySearch
+{
+    public static int Execute(int[] numeros, int tamanho, int alvo)
+    {
+        int esquerda = 0, direita = tamanho - 1;
+
+        while (esquerda <= direita)
+        {
+            int meio = esquerda + (direita - esquerda) / 2;
+
+            if(numeros[meio] == alvo)
+            {
+                return meio;
+            }
+            else if (numeros[meio] < alvo)
+            {
+                esquerda = meio + 1;
+            }
+            else
+            {
+                direita = meio - 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ SortAndSearchAlgoritms/SortAndSearchAlgoritms/Program.cs
@@ -0,0 +1,10 @@
+﻿using SortAndSearchAlgoritms.Algoritms.Search;
+
+int[] numerosOrdenados = { 1,2,3,4,5,6,7,8,9 };
+int[] numerosAleatorios = { 5,1,65,86,41,34,0,45,32 };
+int alvo = 6;
+int resultado;
+
+resultado = BinarySearch.Execute(numerosOrdenados, numerosOrdenados.Length, alvo);
+
+Console.WriteLine(resultado);

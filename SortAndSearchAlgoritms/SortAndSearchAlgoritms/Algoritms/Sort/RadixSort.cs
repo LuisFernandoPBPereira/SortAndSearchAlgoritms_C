d89e95f48f7b0d628ab924044c64e056bb99ffcb
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-11-29 16:59:44 -0300

    feat: RadixSort implementado

---
+++ SortAndSearchAlgoritms/SortAndSearchAlgoritms/Algoritms/Sort/RadixSort.cs
@@ -0,0 +1,52 @@
+﻿namespace SortAndSearchAlgoritms.Algoritms.Sort;
+
+public static class RadixSort
+{
+    public static int[] Execute(int[] numeros)
+    {
+        int maximo = numeros[0];
+
+        for (int i = 1; i < numeros.Length; i++)
+        {
+            if (numeros[i] > maximo)
+            {
+                maximo = numeros[i];
+            }
+        }
+
+        for (int expoente = 1; maximo / expoente > 0; expoente *= 10)
+        {
+            CountSort(numeros, expoente);
+        }
+
+        return numeros;
+    }
+
+    private static void CountSort(int[] numeros, int expoente)
+    {
+        int[] saida = new int[numeros.Length];
+        int[] contador = new int[10];
+        contador[0] = 0;
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            contador[(numeros[i] / expoente) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++)
+        {
+            contador[i] += contador[i-1];
+        }
+
+        for (int i = numeros.Length - 1; i >= 0; i--)
+        {
+            saida[contador[(numeros[i]) / expoente % 10] - 1] = numeros[i];
+            contador[(numeros[i] / expoente) % 10]--;
+        }
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = saida[i];
+        }
+    }
+}
--- SortAndSearchAlgoritms/SortAndSearchAlgoritms/Program.cs
+++ SortAndSearchAlgoritms/SortAndSearchAlgoritms/Program.cs
@@ -3,25 +3,28 @@ using SortAndSearchAlgoritms.Algoritms.Sort;
 
 int[] numerosOrdenados = { 1,2,3,4,5,6,7,8,9 };
 int[] numerosAleatorios = { 5,1,65,86,41,34,0,45,32 };
-float[] numerosAleatorios2 = { 0.78f, 0.17f, 0.39f, 0.26f, 0.72f, 0.94f, 0.21f, 0.12f, 0.23f, 0.68f };
+float[] numerosFloatAleatorios = { 0.78f, 0.17f, 0.39f, 0.26f, 0.72f, 0.94f, 0.21f, 0.12f, 0.23f, 0.68f };
 int alvo = 6;
 
 var buscaBinary = BinarySearch.Execute(numerosOrdenados, alvo);
 var buscaJump = JumpSearch.Execute(numerosOrdenados, alvo);
 var buscaInterpolation = InterpolationSearch.Execute(numerosOrdenados, alvo);
 var buscaTernary = TernarySearch.Execute(numerosOrdenados, alvo);
-var ordenacaoBucket = BucketSort.Execute(numerosAleatorios2);
+var ordenacaoBucket = BucketSort.Execute(numerosFloatAleatorios);
 var ordenacaoMerge = MergeSort.Execute(numerosAleatorios);
 var ordenacaoShell = ShellSort.Execute(numerosAleatorios);
 var ordenacaoBubble = BubbleSort.Execute(numerosAleatorios);
+var ordenacaoRadix = RadixSort.Execute(numerosAleatorios);
 
 
 //ImprimeLista(ordenacaoBucket);
 //ImprimeLista(ordenacaoMerge);
 //ImprimeLista(ordenacaoShell);
 //ImprimeLista(ordenacaoBubble);
+ImprimeLista(ordenacaoRadix);
 
-Console.WriteLine(buscaTernary);
+
+//Console.WriteLine(buscaTernary);
 
 void ImprimeLista<Number>(Number[] numeros)
 {

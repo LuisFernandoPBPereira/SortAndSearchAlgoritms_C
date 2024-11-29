06e4179e94271cacfe9aec7ec5d36ee126fd5836
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-11-29 16:34:18 -0300

    feat: JumpSearch implementado

--- SortAndSearchAlgoritms/SortAndSearchAlgoritms/Algoritms/Search/JumpSearch.cs
+++ SortAndSearchAlgoritms/SortAndSearchAlgoritms/Algoritms/Search/JumpSearch.cs
@@ -2,8 +2,24 @@
 
 public static class JumpSearch
 {
-    public static void Execute()
+    public static int Execute(int[] numeros, int alvo)
     {
+        int step = (int)Math.Sqrt(numeros.Length);
+        int anterior = 0;
 
+        while (numeros[Math.Min(step, numeros.Length) - 1] < alvo)
+        {
+            anterior = step;
+            step += (int)Math.Sqrt(numeros.Length);
+
+            if (anterior >= numeros.Length) return -1;
+        }
+
+        for (int i = anterior; i < Math.Min(step, numeros.Length); i++)
+        {
+            if (numeros[i] == alvo) return i;
+        }
+
+        return -1;
     }
 }
--- SortAndSearchAlgoritms/SortAndSearchAlgoritms/Program.cs
+++ SortAndSearchAlgoritms/SortAndSearchAlgoritms/Program.cs
@@ -5,18 +5,21 @@ int[] numerosOrdenados = { 1,2,3,4,5,6,7,8,9 };
 int[] numerosAleatorios = { 5,1,65,86,41,34,0,45,32 };
 float[] numerosAleatorios2 = { 0.78f, 0.17f, 0.39f, 0.26f, 0.72f, 0.94f, 0.21f, 0.12f, 0.23f, 0.68f };
 int alvo = 6;
-int resultado;
 
-resultado = BinarySearch.Execute(numerosOrdenados, alvo);
+var buscaBinary = BinarySearch.Execute(numerosOrdenados, alvo);
+var buscaJump = JumpSearch.Execute(numerosOrdenados, alvo);
 var ordenacaoBucket = BucketSort.Execute(numerosAleatorios2);
 var ordenacaoMerge = MergeSort.Execute(numerosAleatorios);
 var ordenacaoShell = ShellSort.Execute(numerosAleatorios);
 var ordenacaoBubble = BubbleSort.Execute(numerosAleatorios);
 
+
 //ImprimeLista(ordenacaoBucket);
 //ImprimeLista(ordenacaoMerge);
 //ImprimeLista(ordenacaoShell);
-ImprimeLista(ordenacaoBubble);
+//ImprimeLista(ordenacaoBubble);
+
+Console.WriteLine(buscaJump);
 
 void ImprimeLista<Number>(Number[] numeros)
 {
